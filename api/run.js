7fa54f61e5d0903b87852aa353e424716f541eb0
Shubham Sevda <shubhamsevda@Sevda-MacBook-Pro.local>
2021-05-16 12:19:01 +0530

    replace packages

--- api/run.js
+++ api/run.js
@@ -2,7 +2,7 @@
 // const puppeteer = require('puppeteer-core');
 // const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
 // const pluginStealth = require("puppeteer-extra-plugin-stealth");
-const { chromium } = require('playwright');
+const playwright = require("playwright-aws-lambda")
 
 const util = require("util");
 const request = util.promisify(require("request"));
@@ -186,7 +186,7 @@ const getLinkPreviewAttributes = async (
     puppeteerAgent = "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)"
 ) => {
 
-    const browser = await chromium.launch();
+    const browser = await playwright.launchChromium({ headless: true })
         const context = await browser.newContext();
         const page = await context.newPage();
 
