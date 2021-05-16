a097ec7a0ae8b223d20c1a51f5fb99f069be3126
Shubham Sevda <shubhamsevda@Sevda-MacBook-Pro.local>
2021-05-17 00:41:24 +0530

    heya

--- api/run.js
+++ api/run.js
@@ -12,84 +12,84 @@ const isBase64 = require("is-base64");
 
 // puppeteer.use(pluginStealth());
 // puppeteer.use(AdblockerPlugin());
+//
+// const urlImageIsAccessible = async (url) => {
+//     const correctedUrls = getUrls(url);
+//     if (isBase64(url, { allowMime: true })) {
+//         return true;
+//     }
+//     if (correctedUrls.size !== 0) {
+//         const urlResponse = await request(correctedUrls.values().next().value);
+//         const contentType = urlResponse.headers["content-type"];
+//         return new RegExp("image/*").test(contentType);
+//     }
+// };
 
-const urlImageIsAccessible = async (url) => {
-    const correctedUrls = getUrls(url);
-    if (isBase64(url, { allowMime: true })) {
-        return true;
-    }
-    if (correctedUrls.size !== 0) {
-        const urlResponse = await request(correctedUrls.values().next().value);
-        const contentType = urlResponse.headers["content-type"];
-        return new RegExp("image/*").test(contentType);
-    }
-};
-
-const getImg = async (page, uri) => {
-    const img = await page.evaluate(async () => {
-        const ogImg = (
-            document.querySelector('meta[property="og:image"]')
-        );
-        if (
-            ogImg != null &&
-            ogImg.content.length > 0 &&
-            (await urlImageIsAccessible(ogImg.content))
-        ) {
-            return ogImg.content;
-        }
-        const imgRelLink = (
-            document.querySelector('link[rel="image_src"]')
-        );
-        if (
-            imgRelLink != null &&
-            imgRelLink.href.length > 0 &&
-            (await urlImageIsAccessible(imgRelLink.href))
-        ) {
-            return imgRelLink.href;
-        }
-        const twitterImg = (
-            document.querySelector('meta[name="twitter:image"]')
-        );
-        if (
-            twitterImg != null &&
-            twitterImg.content.length > 0 &&
-            (await urlImageIsAccessible(twitterImg.content))
-        ) {
-            return twitterImg.content;
-        }
-
-        let imgs = Array.from(document.getElementsByTagName("img"));
-        let src;
-        if (imgs.length > 0) {
-            imgs = imgs.filter((img) => {
-                let addImg = true;
-                if (img.naturalWidth > img.naturalHeight) {
-                    if (img.naturalWidth / img.naturalHeight > 3) {
-                        addImg = false;
-                    }
-                } else {
-                    if (img.naturalHeight / img.naturalWidth > 3) {
-                        addImg = false;
-                    }
-                }
-                if (img.naturalHeight <= 50 || img.naturalWidth <= 50) {
-                    addImg = false;
-                }
-                return addImg;
-            });
-            if (imgs.length > 0) {
-                imgs.forEach((img) =>
-                    img.src.indexOf("//") === -1
-                        ? (img.src = `${new URL(uri).origin}/${src}`)
-                        : img.src
-                );
-                return imgs[0].src;
-            }
-        }
-        return null;
-    });
-    return img;
-};
+// const getImg = async (page, uri) => {
+//     const img = await page.evaluate(async () => {
+//         const ogImg = (
+//             document.querySelector('meta[property="og:image"]')
+//         );
+//         if (
+//             ogImg != null &&
+//             ogImg.content.length > 0 &&
+//             (await urlImageIsAccessible(ogImg.content))
+//         ) {
+//             return ogImg.content;
+//         }
+//         const imgRelLink = (
+//             document.querySelector('link[rel="image_src"]')
+//         );
+//         if (
+//             imgRelLink != null &&
+//             imgRelLink.href.length > 0 &&
+//             (await urlImageIsAccessible(imgRelLink.href))
+//         ) {
+//             return imgRelLink.href;
+//         }
+//         const twitterImg = (
+//             document.querySelector('meta[name="twitter:image"]')
+//         );
+//         if (
+//             twitterImg != null &&
+//             twitterImg.content.length > 0 &&
+//             (await urlImageIsAccessible(twitterImg.content))
+//         ) {
+//             return twitterImg.content;
+//         }
+//
+//         let imgs = Array.from(document.getElementsByTagName("img"));
+//         let src;
+//         if (imgs.length > 0) {
+//             imgs = imgs.filter((img) => {
+//                 let addImg = true;
+//                 if (img.naturalWidth > img.naturalHeight) {
+//                     if (img.naturalWidth / img.naturalHeight > 3) {
+//                         addImg = false;
+//                     }
+//                 } else {
+//                     if (img.naturalHeight / img.naturalWidth > 3) {
+//                         addImg = false;
+//                     }
+//                 }
+//                 if (img.naturalHeight <= 50 || img.naturalWidth <= 50) {
+//                     addImg = false;
+//                 }
+//                 return addImg;
+//             });
+//             if (imgs.length > 0) {
+//                 imgs.forEach((img) =>
+//                     img.src.indexOf("//") === -1
+//                         ? (img.src = `${new URL(uri).origin}/${src}`)
+//                         : img.src
+//                 );
+//                 return imgs[0].src;
+//             }
+//         }
+//         return null;
+//     });
+//     return img;
+// };
 
 const getTitle = async (page) => {
     const title = await page.evaluate(() => {
@@ -214,7 +214,7 @@ const getLinkPreviewAttributes = async (
     obj.title = await getTitle(page);
     obj.description = await getDescription(page);
     obj.domain = await getDomainName(page, url);
-    obj.img = await getImg(page, url);
+    // obj.img = await getImg(page, url);
 
     await page.close();
     console.log(11111);
